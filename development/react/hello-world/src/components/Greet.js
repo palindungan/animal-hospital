f5cf116c03257a1c9be13f010d26b39dfbb0dd29
palindungan <rizkika.palindungan1@gmail.com>
2022-07-13 22:52:05 +0700

    day 1

---
+++ development/react/hello-world/src/components/Greet.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// function component without using es6
+// function Greet() {
+//     return (<h1>Hello Rizkika</h1>);
+// }
+
+//// ---------------
+
+const Greet = () => <h1>Hello Rizkika 2</h1>; // function component using es6
+
+export default Greet;
+
+//// ---------------
+
+// export const Greet = () => <h1>Hello Rizkika 3</h1>; // function component using es6
