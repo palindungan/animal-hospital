0e5ec812a91df44705019dbbe014025770a204b3
palindungan <rizkika.palindungan1@gmail.com>
2022-07-20 22:59:26 +0700

    Update NameList.js

--- development/react/hello-world/src/components/NameList.js
+++ development/react/hello-world/src/components/NameList.js
@@ -2,27 +2,31 @@ import React from "react";
 import Person from "./Person";
 
 function NameList() {
-  const persons = [
-    {
-      id: 1,
-      name: "Rizkika",
-      skill: "Frontend",
-    },
-    {
-      id: 2,
-      name: "Zakka",
-      skill: "Backend",
-    },
-    {
-      id: 3,
-      name: "Palindungan",
-      skill: "Fullstack",
-    },
-  ];
-  const personList = persons.map((person) => (
-    <Person key={person.id} person={person}></Person>
-  ));
-  return <div>{personList}</div>;
+  // const persons = [
+  //   {
+  //     id: 1,
+  //     name: "Rizkika",
+  //     skill: "Frontend",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Zakka",
+  //     skill: "Backend",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Palindungan",
+  //     skill: "Fullstack",
+  //   },
+  // ];
+  // const personList = persons.map((person) => (
+  //   <Person key={person.id} person={person}></Person>
+  // ));
+  // return <div>{personList}</div>;
+
+  const names = ["Rizkika", "Zakka", "Palindungan", "Palindungan"];
+  const nameList = names.map((name, index) => <h2 key={index}>{name}</h2>);
+  return nameList;
 }
 
 export default NameList;
