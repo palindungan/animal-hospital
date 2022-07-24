184939c308aa217cb39146afe241983158cca163
palindungan <rizkika.palindungan1@gmail.com>
2022-07-24 16:35:54 +0700

    updateing

--- development/react/hello-world/src/components/LiveCycleA.js
+++ development/react/hello-world/src/components/LiveCycleA.js
@@ -6,28 +6,50 @@ class LiveCycleA extends Component {
     super(props);
 
     this.state = {
-      username: "",
+      username: "Hoho",
     };
 
-    console.log("mounting 1. LiveCycleA constructor");
+    // console.log("mounting 1. LiveCycleA constructor");
   }
 
   static getDerivedStateFromProps(props, state) {
-    console.log("mounting 2. LiveCycleA static getDerivedStateFromProps");
-
+    // console.log("mounting 2. LiveCycleA static getDerivedStateFromProps");
+    console.log("updateing 1. LiveCycleA getDerivedStateFromProps");
     return null;
   }
 
   componentDidMount() {
-    console.log("mounting 4. LiveCycleA componentDidMount");
+    // console.log("mounting 4. LiveCycleA componentDidMount");
   }
 
+  shouldComponentUpdate() {
+    console.log("updateing 2. LiveCycleA shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log("updateing 4. LiveCycleA getSnapshotBeforeUpdate");
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log("updateing 5. LiveCycleA componentDidUpdate");
+  }
+
+  changeState = () => {
+    this.setState({
+      username: "Hehe",
+    });
+  };
+
   render() {
-    console.log("mounting 3. LiveCycleA Render");
+    // console.log("mounting 3. LiveCycleA Render");
+    console.log("updateing 3. LiveCycleA Render");
 
     return (
       <div>
         <div>LiveCycleA</div>
+        <button onClick={this.changeState}>Change State</button>
         <LiveCycleB></LiveCycleB>
       </div>
     );
--- development/react/hello-world/src/components/LiveCycleB.js
+++ development/react/hello-world/src/components/LiveCycleB.js
@@ -12,13 +12,27 @@ class LiveCycleB extends Component {
   }
 
   static getDerivedStateFromProps(props, state) {
-    console.log("mounting 2. LiveCycleB static getDerivedStateFromProps");
-
+    // console.log("mounting 2. LiveCycleB static getDerivedStateFromProps");
+    console.log("updateing 1. LiveCycleB getDerivedStateFromProps");
     return null;
   }
 
   componentDidMount() {
-    console.log("mounting 4. LiveCycleB componentDidMount");
+    // console.log("mounting 4. LiveCycleB componentDidMount");
+  }
+
+  shouldComponentUpdate() {
+    console.log("updateing 2. LiveCycleB shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log("updateing 4. LiveCycleB getSnapshotBeforeUpdate");
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log("updateing 5. LiveCycleB componentDidUpdate");
   }
 
   render() {
