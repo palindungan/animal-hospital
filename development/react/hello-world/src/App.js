c05157f3743ada191fc92afbb34f8f76e153399d
palindungan <rizkika.palindungan1@gmail.com>
2022-07-28 23:33:16 +0700

    Forwarding Refs

--- development/react/hello-world/src/App.js
+++ development/react/hello-world/src/App.js
@@ -24,6 +24,7 @@ import PureComp from "./components/PureComp";
 import ParentComp from "./components/ParentComp";
 import RefsDemo from "./components/RefsDemo";
 import FocusInput from "./components/FocusInput";
+import FRParentInput from "./components/FRParentInput";
 
 function App() {
   return (
@@ -74,7 +75,8 @@ function App() {
       {/* <ParentComp></ParentComp> */}
 
       {/* <RefsDemo></RefsDemo> */}
-      <FocusInput></FocusInput>
+      {/* <FocusInput></FocusInput> */}
+      <FRParentInput></FRParentInput>
     </div>
   );
 }
