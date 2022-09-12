fb32d5633a9fa94ed084b20bfd0d7c601ef2116c
palindungan <rizkika.palindungan1@gmail.com>
2022-09-13 06:35:09 +0700

    51

---
+++ development/react/state-hook/src/components/HookMouse.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+function HookMouse() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const logMousePosition = (e) => {
+    console.log("log mouse position");
+    setX(e.clientX);
+    setY(e.clientY);
+  };
+
+  useEffect(() => {
+    console.log("use effect created");
+    window.addEventListener("mousemove", logMousePosition);
+  }, []);
+
+  return (
+    <div>
+      X - {x}, y - {y}
+    </div>
+  );
+}
+
+export default HookMouse;
