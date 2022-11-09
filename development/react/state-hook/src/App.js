5268b3da2d8b2a1df76e011afbcab48e8ecd3197
palindungan <rizkika.palindungan1@gmail.com>
2022-11-09 23:25:48 +0700

    usereduser usecontex

--- development/react/state-hook/src/App.js
+++ development/react/state-hook/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
 import ClassCounterOne from "./components/ClassCounterOne";
@@ -17,37 +17,63 @@ import HookMouse from "./components/HookMouse";
 import IntervalClassCounter from "./components/IntervalClassCounter";
 import IntervalHookCounter from "./components/IntervalHookCounter";
 import MouseContainer from "./components/MouseContainer";
+import NewComponentA from "./components/NewComponentA";
+import NewComponentB from "./components/NewComponentB";
+import NewComponentC from "./components/NewComponentC";
 
 export const UserContent = React.createContext();
 export const ChannelContext = React.createContext();
 
-function App() {
-  return (
-    <div className="App">
-      {/* <ClassCounter /> */}
-      {/* <HookCounter /> */}
-      {/* <HookCounterTwo /> */}
-      {/* <HookCounterThree /> */}
-      {/* <HookCounterFour /> */}
-      {/* <ClassCounterOne /> */}
-      {/* <HookCounterOne /> */}
-      {/* <ClassMouse /> */}
-      {/* <HookMouse /> */}
-      {/* <MouseContainer /> */}
-      {/* <IntervalClassCounter /> */}
-      {/* <IntervalHookCounter /> */}
-      {/* <DataFetching /> */}
+export const CountContext = React.createContext();
+const reducer = (state, action) => {
+  switch (action) {
+    case "increament":
+      return state + 1;
+    case "decreament":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+const initialState = 0;
 
-      {/* <UserContent.Provider value={"Rizkika"}>
+function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <CountContext.Provider
+      value={{ countState: count, countDispatch: dispatch }}
+    >
+      <div className="App">
+        {/* <ClassCounter /> */}
+        {/* <HookCounter /> */}
+        {/* <HookCounterTwo /> */}
+        {/* <HookCounterThree /> */}
+        {/* <HookCounterFour /> */}
+        {/* <ClassCounterOne /> */}
+        {/* <HookCounterOne /> */}
+        {/* <ClassMouse /> */}
+        {/* <HookMouse /> */}
+        {/* <MouseContainer /> */}
+        {/* <IntervalClassCounter /> */}
+        {/* <IntervalHookCounter /> */}
+        {/* <DataFetching /> */}
+        {/* <UserContent.Provider value={"Rizkika"}>
         <ChannelContext.Provider value={"HEHE"}>
           <ComponentC />
         </ChannelContext.Provider>
       </UserContent.Provider> */}
-
-      {/* <CounterOne></CounterOne> */}
-      {/* <CounterTwo></CounterTwo> */}
-      <CounterThree></CounterThree>
-    </div>
+        {/* <CounterOne></CounterOne> */}
+        {/* <CounterTwo></CounterTwo> */}
+        {/* <CounterThree></CounterThree> */}
+        Count : {count}
+        <NewComponentA></NewComponentA>
+        <NewComponentB></NewComponentB>
+        <NewComponentC></NewComponentC>
+      </div>
+    </CountContext.Provider>
   );
 }
 
---
+++ development/react/state-hook/src/components/NewComponentA.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { CountContext } from "../App";
+
+function NewComponentA() {
+  const countContext = useContext(CountContext);
+  return (
+    <div>
+      Count : {countContext.countState}
+      <button onClick={() => countContext.countDispatch("increament")}>
+        Increament
+      </button>
+      <button onClick={() => countContext.countDispatch("decreament")}>
+        Decreament
+      </button>
+      <button onClick={() => countContext.countDispatch("reset")}>Reset</button>
+    </div>
+  );
+}
+
+export default NewComponentA;
