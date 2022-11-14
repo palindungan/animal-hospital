1e6b47330a51057cc8c003f17c8af622e1825c0a
palindungan <rizkika.palindungan1@gmail.com>
2022-11-14 22:51:40 +0700

    68

--- development/react/state_hook/src/App.js
+++ development/react/state_hook/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import DataFechingOne from "./components/DataFechingOne";
+import DataFechingTwo from "./components/DataFechingTwo";
 
 function App() {
   return (
     <div className="App">
-      <DataFechingOne />
+      {/* <DataFechingOne /> */}
+      <DataFechingTwo />
     </div>
   );
 }
