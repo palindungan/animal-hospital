40428bdb86a15864a67c7cd42f5f40a6a9e80f89
palindungan <rizkika.palindungan1@gmail.com>
2022-11-15 01:51:11 +0700

    69

--- development/react/state_hook/src/App.js
+++ development/react/state_hook/src/App.js
@@ -2,12 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import DataFechingOne from "./components/DataFechingOne";
 import DataFechingTwo from "./components/DataFechingTwo";
+import ParentComponent from "./components/ParentComponent";
 
 function App() {
   return (
     <div className="App">
       {/* <DataFechingOne /> */}
-      <DataFechingTwo />
+      <ParentComponent />
     </div>
   );
 }
