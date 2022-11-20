89464435eafd3220491efc6063cab0f50a6e793e
palindungan <rizkika.palindungan1@gmail.com>
2022-11-21 00:20:47 +0700

    71

--- development/react/state_hook/src/App.js
+++ development/react/state_hook/src/App.js
@@ -4,13 +4,15 @@ import DataFechingOne from "./components/DataFechingOne";
 import DataFechingTwo from "./components/DataFechingTwo";
 import ParentComponent from "./components/ParentComponent";
 import Counter from "./components/Counter";
+import FocusInput from "./components/FocusInput";
 
 function App() {
   return (
     <div className="App">
       {/* <DataFechingOne /> */}
       {/* <ParentComponent /> */}
-      <Counter />
+      {/* <Counter /> */}
+      <FocusInput />
     </div>
   );
 }
