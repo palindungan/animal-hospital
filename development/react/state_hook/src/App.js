7a21934b5538ea03a3f60dbb00e83f4684bcd82a
palindungan <rizkika.palindungan1@gmail.com>
2022-11-22 03:03:29 +0700

    76

--- development/react/state_hook/src/App.js
+++ development/react/state_hook/src/App.js
@@ -11,6 +11,7 @@ import DocTitleOne from "./components/DocTitleOne";
 import DocTitleTwo from "./components/DocTitleTwo";
 import CounterOne from "./components/CounterOne";
 import CounterTwo from "./components/CounterTwo";
+import UserForm from "./components/UserForm";
 
 function App() {
   return (
@@ -23,8 +24,9 @@ function App() {
       {/* <HookTimer /> */}
       {/* <DocTitleOne /> */}
       {/* <DocTitleTwo /> */}
-      <CounterOne />
-      <CounterTwo />
+      {/* <CounterOne /> */}
+      {/* <CounterTwo /> */}
+      <UserForm />
     </div>
   );
 }
