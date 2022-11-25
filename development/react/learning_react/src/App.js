cbfdb579315c631ede708c719f4b6620dda446b8
palindungan <rizkika.palindungan1@gmail.com>
2022-11-26 04:07:32 +0700

    80

--- development/react/learning_react/src/App.js
+++ development/react/learning_react/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { UseState } from "./components_react_render/UseState/UseState";
+import { UseReducer } from "./components_react_render/UseReducer/UseReducer";
 
 function App() {
   return (
     <div className="App">
-      <UseState />
+      {/* <UseState /> */}
+      <UseReducer />
     </div>
   );
 }
