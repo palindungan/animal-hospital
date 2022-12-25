aab79449bdf58a900f43d2e5d95578afa314edba
palindungan <rizkika.palindungan1@gmail.com>
2022-12-25 15:56:25 +0700

    99

--- development/react/react-typescript-demo/src/App.tsx
+++ development/react/react-typescript-demo/src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css';
 import { Button } from './components/Button';
+import { Container } from './components/Container';
 import { Input } from './components/Input';
 
 function App() {
   return (
     <div className="App">
-      <Button handleClick={(event, id) => {
+      {/* <Button handleClick={(event, id) => {
         console.log("Button Clicked", event, id);
       }} />
-      <Input value="" handleChange={(event) => console.log(event)}></Input>
+      <Input value="" handleChange={(event) => console.log(event)}></Input> */}
+
+      <Container styles={{ border: '1px solid black', padding: '1rem' }}></Container>
     </div>
   );
 }
