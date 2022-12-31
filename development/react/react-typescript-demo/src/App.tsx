79b6fd825bdccf9d38c605d4ccbb505ab83229c4
palindungan <rizkika.palindungan1@gmail.com>
2022-12-31 19:18:50 +0700

    101

--- development/react/react-typescript-demo/src/App.tsx
+++ development/react/react-typescript-demo/src/App.tsx
@@ -1,34 +1,10 @@
 import './App.css';
-import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
+import { LoggedIn } from './components/state/LoggedIn';
 
 function App() {
-  const personName = {
-    first:"rizkika",
-    last:"zakka"
-  };
-
-  const nameList = [
-    {
-      first:"1 1",
-      last:"1 2"
-    },
-    {
-      first:"2 1",
-      last:"2 2"
-    },
-    {
-      first:"3 1",
-      last:"3 2"
-    },
-  ];
-
   return (
     <div className="App">
-      <Greet name="Rizkika" messageCount={20} isLoggedIn={false}></Greet>
-      <Person name={personName}></Person>
-      <PersonList names={nameList}></PersonList>
+      <LoggedIn></LoggedIn>
     </div>
   );
 }
