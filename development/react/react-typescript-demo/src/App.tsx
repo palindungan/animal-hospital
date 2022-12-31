34db44510d6816e792758f05d8a74b306c341a9f
palindungan <rizkika.palindungan1@gmail.com>
2022-12-31 19:47:31 +0700

    102

--- development/react/react-typescript-demo/src/App.tsx
+++ development/react/react-typescript-demo/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { LoggedIn } from './components/state/LoggedIn';
+import { User } from './components/state/User';
 
 function App() {
   return (
     <div className="App">
-      <LoggedIn></LoggedIn>
+      {/* <LoggedIn></LoggedIn> */}
+      <User></User>
     </div>
   );
 }
