7cc505e7029aa589e5598922b013074e4463a09c
palindungan <rizkika.palindungan1@gmail.com>
2023-01-02 17:03:01 +0700

    110

--- development/react/react-typescript-demo/src/App.tsx
+++ development/react/react-typescript-demo/src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css';
+import { Private } from './components/auth/Private';
+import { Profile } from './components/auth/Profile';
 import { Counter } from './components/class/Counter';
 import { User } from './components/context/User';
 import { UserContextProvider } from './components/context/UserContext';
@@ -10,7 +12,9 @@ function App() {
         <User></User>
       </UserContextProvider> */}
 
-      <Counter message='The Count Vale is '></Counter>
+      {/* <Counter message='The Count Vale is '></Counter> */}
+
+      <Private isLoggedIn={true} component={Profile}></Private>
     </div>
   );
 }
