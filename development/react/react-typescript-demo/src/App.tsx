dbf9ba13ed754d8f2e6fdc01fa64b26983b990ed
palindungan <rizkika.palindungan1@gmail.com>
2023-01-03 09:42:14 +0700

    113

--- development/react/react-typescript-demo/src/App.tsx
+++ development/react/react-typescript-demo/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { RandomNumber } from './components/restriction/RandomNumber';
+import { Toast } from './components/templateliterals/Toast';
 
 function App() {
   return (
     <div className="App">
-      <RandomNumber value={10} isPositive />
+      {/* <RandomNumber value={10} isPositive /> */}
+      <Toast position="left-center"></Toast>
     </div>
   );
 }
