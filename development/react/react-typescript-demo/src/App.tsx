9fa0a596065402c3028ddfb72f6cb9dba73ea553
palindungan <rizkika.palindungan1@gmail.com>
2023-01-03 11:41:57 +0700

    116

--- development/react/react-typescript-demo/src/App.tsx
+++ development/react/react-typescript-demo/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { CustomButton } from './components/html/Button';
+import { Text } from './components/polymorphic/Text';
 import { RandomNumber } from './components/restriction/RandomNumber';
 import { Toast } from './components/templateliterals/Toast';
 
@@ -9,9 +10,13 @@ function App() {
       {/* <RandomNumber value={10} isPositive /> */}
       {/* <Toast position="left-center"></Toast> */}
 
-      <CustomButton variant="primary" onClick={() => console.log("clicked")}>
+      {/* <CustomButton variant="primary" onClick={() => console.log("clicked")}>
         Primary Button
-      </CustomButton>
+      </CustomButton> */}
+
+      <Text as="h1" size="lg">Heading</Text>
+      <Text as="p" size="md">Parahraph</Text>
+      <Text as="label" htmlFor="someId" size="sm" color="secondary">Label</Text>
     </div>
   );
 }
